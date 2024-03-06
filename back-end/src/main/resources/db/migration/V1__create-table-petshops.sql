55a2cb0162f9cb048f00bc02a8d26adf89a82735
pedrobarroso01 <pedrobarrosocontato@gmail.com>
2024-03-05 22:21:47 -0300

    Fixing migration problem

--- back-end/src/main/resources/db/migration/V1__create-table-petshops.sql
+++ back-end/src/main/resources/db/migration/V1__create-table-petshops.sql
@@ -4,6 +4,6 @@ CREATE TABLE petshops (
     distancia int not null,
     precoPequeno float not null,
     precoGrande float not null,
-    precoPequenoFDS float null default 0.0,
-    precoGrandeFDS float null default 0.0
+    precoPequenoFDS float  not null,
+    precoGrandeFDS float  not null
 );
